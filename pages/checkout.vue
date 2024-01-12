9d7a361977ec6f5df32aedd1e2a1011c592b1de8
John Doe <alpercekinmez7@gmail.com>
2024-01-12 09:42:21 -0800

    remove leemnts from cart

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -5,6 +5,10 @@
     <PrismicImage :field="item.data.image" class=" h-[100px] w-[100px] border object-cover"/><div>
     <div :style="{'color':item.data.color}" class="flex justify-start font-cooperbt text-xl">{{ String.fromCharCode(	0x2776 + i) }}<prismic-rich-text :field="item.data.title" class="" /></div>
     <div class="pt-5 font-garamond">€{{item.data.price}},–</div></div>
+    <form @submit="remove($event)" class="pt-8">
+          <input type="hidden" name="id" :value="item.id" />
+          <button class="font-metrik text-xs border border-black rounded-full py-1 px-2 hover:bg-sky-200" type="submit">Remove</button>
+        </form>
   </div>
         <form @submit="handleSubmit($event)" action="/.netlify/functions/create-checkout" method="post" class="pt-8">
           <input type="hidden" name="sku" value="DEMO002" />
@@ -40,6 +44,19 @@
       }
     },
     methods: {
+        remove(event){
+          event.preventDefault();
+          const form = new FormData(event.target)
+          const target = form.get('id')
+          const itemarray = JSON.parse(localStorage.orders)
+          var index = itemarray.indexOf(target);
+          if (index !== -1) {
+          itemarray.splice(index, 1);
+          }
+          event.target.parentElement.remove()
+          localStorage.orders = JSON.stringify(itemarray)
+        },
+
         format(amount, currency) {
         return new Intl.NumberFormat('en-US', {
           style: 'currency',
@@ -82,6 +99,7 @@
   mounted(){
     if(localStorage.orders){
       const targetorders = JSON.parse(localStorage.orders)
+      console.log(targetorders)
     for (let i = 0; i < this.page.results.length; i++) {
       for (let j = 0; j < targetorders.length; j++) {
         if(this.page.results[i].id == targetorders[j]){
