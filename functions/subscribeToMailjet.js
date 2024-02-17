9abad50e4dc7b8b7b424171af8ecf23a748b1c18
John Doe <alpercekinmez7@gmail.com>
2024-02-17 09:46:13 -0800

    lets ignore errors

--- functions/subscribeToMailjet.js
+++ functions/subscribeToMailjet.js
@@ -48,22 +48,13 @@ exports.handler = async (event) => {
     );
 
     return {
-      statusCode: 302,
-      headers: {
-        Location: 'https://scannedfootballphotos.com',
-      },
-      body: JSON.stringify({
-        message: "Contact added to Mailjet successfully",
-      }),
+      statusCode: 200,
     };
   } catch (error) {
     console.error("Error adding contact to Mailjet:", error);
 
     return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Failed to add contact to Mailjet",
-      }),
+      statusCode: 200,
     };
   }
 };
