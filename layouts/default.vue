1771435ee5d143a5d8230782469df6cd897d3809
John Doe <alpercekinmez7@gmail.com>
2024-02-21 14:25:02 -0800

    dfa

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,24 +6,6 @@
       :settings="settings"
     />
     <main><nuxt /></main>
-    <div v-if="this.$route.name != 'emailsuccess'" id="alper" class="fixed w-screen h-screen bg-white z-10 transition-[bottom] ease-in duration-[2000ms] delay-[2000ms] overflow-hidden bottom-0">
-      <div :style="{'color':settings.data.splashcolor}" class="absolute z-20 inset-0 flex items-center justify-center font-cooperbt flex-col p-5">
-    <div class="px-2.5 py-2 md:p-5 fixed top-0 left-0 puff pointer-events-none">
-    <div class="flex flex-wrap items-start justify-between md:gap-x-10 gap-y-3 leading-none">
-      <div class="text-xl md:text-2xl !no-underline font-platform">
-        {{ $prismic.asText(settings.data.siteTitle) }}
-      </div>
-    </div>
-    </div>
-        <prismic-rich-text :field="settings.data.splashtexttop" class="text-center"/>
-        <prismic-rich-text :field="settings.data.splashnumber" class="text-center"/>
-        <prismic-rich-text :field="settings.data.splashtextbottom" class="text-center"/>
-      </div>
-      <div class="absolute z-10">
-        <PrismicImage :field="settings.data.splashimage" class="h-screen w-screen object-cover hidden md:block"/>
-        <PrismicImage :field="settings.data.splashimagephone" class="h-screen w-screen object-cover md:hidden"/>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -37,16 +19,6 @@ export default {
       return this.$store.state.prismic.settings
     }
   },
-  mounted() {
-    const alper = document.getElementById('alper')
-    if (alper) {
-    alper.style.bottom = '100vh'
-    setTimeout(() => {
-      alper.remove()
-      document.getElementById('marquee-slider-text').style.display = 'block'
-    }, 4000);
-  }
-}
 }
 </script>
 
