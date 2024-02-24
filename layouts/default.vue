a3eb83e591ba1e953c197dab73a5437f6dd32172
John Doe <alpercekinmez7@gmail.com>
2024-02-24 05:32:12 -0800

    maye

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
       :settings="settings"
     />
     <main><nuxt /></main>
-    <div v-if="splash" id="alper" class="fixed w-screen h-screen bg-white z-10 transition-[bottom] ease-in duration-[2000ms] delay-[2000ms] overflow-hidden bottom-0">
+    <div v-if="splash" id="alper" class="pointer-events-none fixed opacity-0 w-screen h-screen bg-white z-10 transition-[bottom] ease-in duration-[2000ms] delay-[2000ms] overflow-hidden bottom-0">
       <div :style="{'color':settings.data.splashcolor}" class="absolute z-20 inset-0 flex items-center justify-center font-cooperbt flex-col p-5">
     <div class="px-2.5 py-2 md:p-5 fixed top-0 left-0 puff pointer-events-none">
     <div class="flex flex-wrap items-start justify-between md:gap-x-10 gap-y-3 leading-none">
@@ -51,6 +51,7 @@ export default {
   mounted() {
     if (this.splash) {
     const alper = document.getElementById('alper')
+    alper.style.opacity = '1'
     alper.style.bottom = '100vh'
     setTimeout(() => {
       alper.style.display = 'none'
