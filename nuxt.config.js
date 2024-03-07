8054200b3da2ff92580e9675079cae4c988c0d81
John Doe <alpercekinmez7@gmail.com>
2024-03-07 07:09:09 -0800

    favico

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
       },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    //link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    link: [{ rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png" }, {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"}, {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"}, {rel:"manifest", href:"/site.webmanifest"}, {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#5bbad5"},]
   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
